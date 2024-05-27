1375cf1a8f93573962d6566a89f539b636e02a04
pavanSaberjack <itagi75@gmail.com>
2024-05-27 15:58:44 +0530

    Added prompts.js file

---
+++ src/modules/gen-ai/OpenAI/data/prompts.js
@@ -0,0 +1,22 @@
+const prompts = {
+  solid_principle:
+    'ACT as a pair programmer who is helping me in improving my code. Follow the rules. \
+    \
+    1. Only share code snippets. \
+    2. Anything other than a code snippet should be embedded between "/*" and "*/" \
+    3. Avoid explanation \
+    \
+    Improve the below code snippet to adhere to the SOLID principle',
+
+  unit_test:
+    'ACT as a pair programmer who is helping me in improving my code. Follow the rules. \
+    \
+    1. Only share code snippets.\
+    2. Anything other than a code snippet should be embedded between "/*" and "*/"\
+    3. Avoid explanation\
+    \
+    Write Unit tests for the below iOS code snippet\
+    ',
+};
+
+module.exports = prompts;
