c4a46a15146bc4afd8165c8da5041708d048afc6
pavanSaberjack <itagi75@gmail.com>
2024-05-28 16:03:57 +0530

    Added jsonwebtoken bcryptjs and auth.js file for validating token

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const auth = (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer", "");
+
+  if (!token) {
+    return res.status(401).send("Access Denied. No token provided");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).send("Invalid Token");
+  }
+};
+
+module.exports = auth;
