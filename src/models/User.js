4898f8fcba7c947af90a7b790d87dcca836e68b6
pavanSaberjack <itagi75@gmail.com>
2024-05-28 19:36:17 +0530

    Added user routes and model for user

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Pre-save middleware to hash password before saving the user document
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ src/routes/users.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+router.post("/register", register);
+
+// POST /users/login - Authenticate user and return token
+router.post("/login", login);
+
+async function register(req, res) {
+  try {
+    let { email, password } = req.body;
+
+    let user = new User({ email, password });
+    console.log("crated new user object");
+    await user.save();
+    console.log("saved new user object");
+
+    // Generate a token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(201).json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+}
+
+module.exports = router;
