98360d419ddc55e1085946b232300f97e138e595
pavanSaberjack <itagi75@gmail.com>
2024-05-28 19:37:17 +0530

    Added DB connection code at the beginning and routing user registration and login routes, testing of protected URLs

--- src/server.js
+++ src/server.js
@@ -3,15 +3,32 @@ const bodyParser = require("body-parser");
 
 // Routers
 const codegenRouter = require("./routes/codegenRouter");
+const userRoutes = require("./routes/users");
+
+// Authentication
+const auth = require("./middleware/authentication/auth");
+
+// Database
+const db = require("./modules/storage/db");
 
 const app = express();
 const port = 3000; // You can choose any port that is available
+
+//  Connect to DB
+db.connectDB();
 
 app.use(bodyParser.json());
 
 // Route handler for GET requests to '/'
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+// Define Routes
+app.use("/api/users", userRoutes);
+
+app.get("/api/protected", auth, (req, res) => {
+  res.send("Access granted to protected resource");
 });
 
 // Use routers
